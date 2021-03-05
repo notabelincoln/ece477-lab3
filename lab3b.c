78c8cf861d6b3f92f9fbaacb569296de3c0a8d43
notabelincoln <abejordandx@gmail.com>
2021-03-05 18:19:18 +0000

    Removed unnecessary preprocessor directives in lab3a.c and lab3b.c

--- lab3b.c
+++ lab3b.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include "wiringPi.h"
-#include "linux/gpio.h"
-#include "sys/ioctl.h"
-#include "string.h"
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <fcntl.h>
+#include <string.h>
 #include <errno.h>
+
+#include "wiringPi.h"
 
 #define BUFFER_SIZE 32
 
